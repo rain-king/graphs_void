5fbc0d32275cdc5cf513aa204badc597204969d8
Murillo Vega Gustavo <g.murillo24@info.uas.edu.mx>
2025-03-06 09:02:04 -0700

    cambios aula B

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gcc *.c -fsanitize=address,undefined,leak -Wpedantic -O0
+# gcc *.c -fsanitize=address,undefined,leak -Wpedantic -O0
+if [ $1 = graph ]; then
+	gcc graph.c linked_list.c main_graph.c -fsanitize=leak -Wall -O1
+elif [ $1 = ll ]; then
+	gcc linked_list.c main_linked_list.c -fsanitize=leak -Wall -O1
+else
+	echo 'Needs argument graph or ll'
+fi
